482ed0a7b3bd6a79f99747eef772bad74fcc620f
Aaron Ash <aaron.ash@gmail.com>
2015-01-23 09:41:55 +0800

    Added some useful shit

---
+++ usefulshit.ps1
@@ -0,0 +1,12 @@
+
+#How to grep a fucking log (jesus fuck this is retarded)
+Get-EventLog MyLogName | Select Message | Select-String FooBar
+
+
+#How to tail a file properly
+gc -tail 10 -wait somefile.log
+#But, that only updates when a file close event happens, or weirdly on some timer crap.
+#This is due to windows filesystem being a piece of shit™
+#So in a separate window, run this over and over again:
+gc -tail 1 somefile.log
+#AA TODO: Write a simple script that does that in the background 
